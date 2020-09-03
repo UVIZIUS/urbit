eb06eb81fb2c0e70c86f3aebdb039dbca637cc3f
Brendan Hay <brendan.g.hay@gmail.com>
2020-09-03 16:55:25 +0200

    build: removing cc from stdenv for trivial derivations

--- nix/new/pkgs/ca-bundle/default.nix
+++ nix/new/pkgs/ca-bundle/default.nix
@@ -1,11 +1,11 @@
-{ stdenv, xxd, cacert }:
+{ stdenvNoCC, xxd, cacert }:
 
-stdenv.mkDerivation {
+stdenvNoCC.mkDerivation {
   name = "ca-bundle";
 
   nativeBuildInputs = [ cacert xxd ];
 
-  phases = [ "installPhase" "fixupPhase" ];
+  phases = [ "installPhase" ];
 
   installPhase = ''
     if ! [ -f "$SSL_CERT_FILE" ]; then
@@ -14,12 +14,13 @@ stdenv.mkDerivation {
     fi
 
     mkdir ./include
+
     cat $SSL_CERT_FILE > include/ca-bundle.crt
     xxd -i include/ca-bundle.crt > ca-bundle.h
+
     mkdir -p $out/include
     mv ca-bundle.h $out/include/
   '';
 
   preferLocalBuild = true;
 }
-
--- nix/new/pkgs/ivory-header/default.nix
+++ nix/new/pkgs/ivory-header/default.nix
@@ -1,16 +1,17 @@
-{ stdenv, fetch-github-lfs, xxd }:
+{ stdenvNoCC, fetch-github-lfs, xxd }:
 
-stdenv.mkDerivation {
+stdenvNoCC.mkDerivation {
   name = "ivory-header";
   src = fetch-github-lfs { src = ../../../../bin/ivory.pill; };
 
   nativeBuildInputs = [ xxd ];
 
-  phases = [ "installPhase" "fixupPhase" ];
+  phases = [ "installPhase" ];
 
   installPhase = ''
     cat $src > u3_Ivory.pill
     xxd -i u3_Ivory.pill > ivory.h
+
     mkdir -p $out/include
     mv ivory.h $out/include/
   '';
