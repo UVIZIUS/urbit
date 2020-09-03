35ba7e10bc4be51bf84616a5dc213a5b53d2eea0
Brendan Hay <brendan.g.hay@gmail.com>
2020-09-03 16:58:47 +0200

    build: correcting erroneous import path

--- nix/new/lib/default.nix
+++ nix/new/lib/default.nix
@@ -2,5 +2,5 @@
 
 {
   fetch-github-lfs = import ./fetch-github-lfs.nix { inherit pkgs; };
-  push-gcp-object = import ./push-gcp-object { inherit pkgs; };
+  push-gcp-object = import ./push-gcp-object.nix { inherit pkgs; };
 }
